b6437832b3659810112513e8d99f70bb16c77ab2
dgp-web <dgp-web@outlook.de>
2021-03-09 11:26:03 +0100

    Start refactoring dispatch$

--- src/functions/dispatch$.ts
+++ src/functions/dispatch$.ts
@@ -3,11 +3,15 @@ import {
     AddEntityActionParamsMap,
     ClearEntityActionParamsList,
     CompositeEntityActionPayload,
-    RemoveEntityActionParamsMap, SelectEntityActionParamsMap, SetEntityActionParamsMap, UpdateEntityActionParamsMap
+    RemoveEntityActionParamsMap,
+    SelectEntityActionParamsMap,
+    SetEntityActionParamsMap,
+    UpdateEntityActionParamsMap
 } from "entity-store/src/models";
 import {
     addEntitiesToState,
-    removeEntitiesFromState, selectEntitiesInState,
+    removeEntitiesFromState,
+    selectEntitiesInState,
     setEntitiesInState,
     updateEntitiesInState
 } from "entity-store/src/functions";
@@ -18,46 +22,41 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
     action: CompositeEntityActionPayload<TEntityTypeMap, null>
 ): Promise<void> {
 
-
-    // Map of entityKey to PutDocument
+    /**
+     * The total result of this operation
+     */
     const entityTypeBulkUpdateMap: KVS<PouchDB.Core.PutDocument<any>> = {};
 
-    // -----
-    let result1;
-    let entity;
-
+    /**
+     * Store all docs or bulk-get results when querying the DB
+     */
+    let queryResult: PouchDB.Core.AllDocsResponse<any> | PouchDB.Core.BulkGetResponse<any>;
+    /**
+     * Store entities for processing in the intermediate steps
+     */
+    let entityCache: Array<any>;
 
     // Clear
     if (action.clear) {
         const entityTypesToClear = action.clear as ClearEntityActionParamsList<TEntityTypeMap, any>;
-        let result = await dbConnection.allDocs<any>({keys: entityTypesToClear as Array<string>});
-        result.rows.forEach(x => {
+        let queryResult = await dbConnection.allDocs<any>({keys: entityTypesToClear as Array<string>});
+        queryResult.rows.forEach(x => {
             entityTypeBulkUpdateMap[x.id] = {_id: x.id, _deleted: true};
         });
     }
 
     // Remove
     if (action.remove) {
-
         const entityTypesToRemoveKeysMap = action.remove as RemoveEntityActionParamsMap<TEntityTypeMap, any>;
         const entityTypesFromWhichToRemoveKeys = Object.keys(entityTypesToRemoveKeysMap);
-        result1 = await dbConnection.bulkGet({
-            docs: entityTypesFromWhichToRemoveKeys.map(x => {
-                return {id: x} as any;
-            })
-        });
-        entity = _.flatten(result1.results
-            .map(x => {
-                return x.docs.map(doc => {
-
-                    const result: PouchDB.Core.GetMeta & PouchDB.Core.IdMeta = (doc as any).ok;
-                    return result as any;
-                });
-
-            }));
+        queryResult = await dbConnection.bulkGet({docs: entityTypesFromWhichToRemoveKeys.map(x => ({id: x}))});
+        entityCache = _.flatten(queryResult.results.map(x => x.docs.map(doc => {
+            const result: PouchDB.Core.GetMeta & PouchDB.Core.IdMeta = (doc as any).ok;
+            return result as any;
+        })));
         entityTypesFromWhichToRemoveKeys.map(async entityType => {
             const idsToRemove = entityTypesToRemoveKeysMap[entityType];
-            const currentState = entity.find(x => x._id === entityType) as any;
+            const currentState = entityCache.find(x => x._id === entityType) as any;
             entityTypeBulkUpdateMap[entityType] = removeEntitiesFromState(currentState as any, idsToRemove);
         });
     }
@@ -67,12 +66,12 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
 
         const entityTypesToSetKeysMap = action.set as SetEntityActionParamsMap<TEntityTypeMap, any>;
         const entityTypesFromWhichToSetKeys = Object.keys(entityTypesToSetKeysMap);
-        result1 = await dbConnection.bulkGet({
+        queryResult = await dbConnection.bulkGet({
             docs: entityTypesFromWhichToSetKeys.map(x => {
                 return {id: x} as any;
             })
         });
-        entity = _.flatten(result1.results
+        entityCache = _.flatten(queryResult.results
             .map(x => {
                 return x.docs.map(doc => {
 
@@ -83,7 +82,7 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
             }));
         entityTypesFromWhichToSetKeys.map(entityType => {
             const entityToSetKVS = entityTypesToSetKeysMap[entityType];
-            const currentState = entity.find(x => x._id === entityType) as any;
+            const currentState = entityCache.find(x => x._id === entityType) as any;
             entityTypeBulkUpdateMap[entityType] = setEntitiesInState(currentState as any, entityToSetKVS);
         });
     }
@@ -93,12 +92,12 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
 
         const entityTypesToAddKeysMap = action.add as AddEntityActionParamsMap<TEntityTypeMap, any>;
         const entityTypesFromWhichToAddKeys = Object.keys(entityTypesToAddKeysMap);
-        result1 = await dbConnection.bulkGet({
+        queryResult = await dbConnection.bulkGet({
             docs: entityTypesFromWhichToAddKeys.map(x => {
                 return {id: x} as any;
             })
         });
-        entity = _.flatten(result1.results
+        entityCache = _.flatten(queryResult.results
             .map(x => {
                 return x.docs.map(doc => {
 
@@ -109,7 +108,7 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
             }));
         entityTypesFromWhichToAddKeys.map(entityType => {
             const entityToAddKVS = entityTypesToAddKeysMap[entityType];
-            const currentState = entity.find(x => x._id === entityType) as any;
+            const currentState = entityCache.find(x => x._id === entityType) as any;
             entityTypeBulkUpdateMap[entityType] = addEntitiesToState(currentState as any, entityToAddKVS);
         });
     }
@@ -119,12 +118,12 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
 
         const entityTypesToUpdateKeysMap = action.update as UpdateEntityActionParamsMap<TEntityTypeMap, any>;
         const entityTypesFromWhichToUpdateKeys = Object.keys(entityTypesToUpdateKeysMap);
-        result1 = await dbConnection.bulkGet({
+        queryResult = await dbConnection.bulkGet({
             docs: entityTypesFromWhichToUpdateKeys.map(x => {
                 return {id: x} as any;
             })
         });
-        entity = _.flatten(result1.results
+        entityCache = _.flatten(queryResult.results
             .map(x => {
                 return x.docs.map(doc => {
 
@@ -135,7 +134,7 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
             }));
         entityTypesFromWhichToUpdateKeys.map(entityType => {
             const entityToUpdateKVS = entityTypesToUpdateKeysMap[entityType];
-            const currentState = entity.find(x => x._id === entityType) as any;
+            const currentState = entityCache.find(x => x._id === entityType) as any;
             entityTypeBulkUpdateMap[entityType] = updateEntitiesInState(currentState as any, entityToUpdateKVS);
         });
     }
@@ -144,12 +143,12 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
     if (action.select) {
         const entityTypesToSelectKeysMap = action.select as SelectEntityActionParamsMap<TEntityTypeMap, any>;
         const entityTypesFromWhichToSelectKeys = Object.keys(entityTypesToSelectKeysMap);
-        result1 = await dbConnection.bulkGet({
+        queryResult = await dbConnection.bulkGet({
             docs: entityTypesFromWhichToSelectKeys.map(x => {
                 return {id: x} as any;
             })
         });
-        entity = _.flatten(result1.results
+        entityCache = _.flatten(queryResult.results
             .map(x => {
                 return x.docs.map(doc => {
 
@@ -160,7 +159,7 @@ export async function dispatch$<TEntityTypeMap extends EntityTypeMap>(
             }));
         entityTypesFromWhichToSelectKeys.map(entityType => {
             const entityToSelect = entityTypesToSelectKeysMap[entityType];
-            const currentState = entity.find(x => x._id === entityType) as any;
+            const currentState = entityCache.find(x => x._id === entityType) as any;
             entityTypeBulkUpdateMap[entityType] = selectEntitiesInState(currentState as any, entityToSelect);
         });
     }
