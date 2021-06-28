6823ccbd679196e408c822866606160c2b28b225
dgp-web <dgp-web@outlook.de>
2021-06-28 17:20:36 +0200

    Move to jest

--- src/functions/effects/__tests__/create-close-db-effect.function.spec.ts
+++ src/functions/effects/__tests__/create-close-db-effect.function.spec.ts
@@ -1,19 +1,40 @@
 import { createCloseDbEffect } from "../create-close-db-effect.function";
-import { Observable } from "rxjs";
-import { CloseDbTimer, DbConnectionSource } from "../../../models";
-import { createCloseDbTimer } from "../../factories/create-close-db-timer.function";
+import { Observable, ReplaySubject } from "rxjs";
+import { DbConnectionInfo, DbConnectionSource } from "../../../models";
 import { createDbConnectionSource } from "../../factories/create-db-connection-source.function";
+import { first } from "rxjs/operators";
+import { createClosingDbConnectionInfo } from "../../factories/create-closing-db-connection-info.function";
+import * as PouchDB from "pouchdb";
+
+const uuidv4 = require("uuid/v4");
+
+export function createGuid() {
+    return uuidv4();
+}
+
+export type TestCloseDbTimer = ReplaySubject<number>;
+
+export function createTestCloseDbTimer(): TestCloseDbTimer {
+    return new ReplaySubject<number>(1);
+}
+
+PouchDB.plugin(require("pouchdb-adapter-memory"));
+
+export function createTestPouchDb(): PouchDB.Database {
+    return new PouchDB(createGuid(), {adapter: "memory"});
+}
 
 describe("createCloseDbEffect", () => {
 
-    let closeDbTimer$: CloseDbTimer;
+    let closeDbTimer$: TestCloseDbTimer;
     let dbConnectionSource$: DbConnectionSource;
 
     let effect: Observable<void>;
-
+    let db: PouchDB.Database;
 
     beforeEach(() => {
-        closeDbTimer$ = createCloseDbTimer();
+        db = createTestPouchDb();
+        closeDbTimer$ = createTestCloseDbTimer();
         dbConnectionSource$ = createDbConnectionSource();
 
         effect = createCloseDbEffect({closeDbTimer$, dbConnectionSource$});
@@ -30,6 +51,18 @@ describe("createCloseDbEffect", () => {
     it(`that reacts when a closeDbTimer is passed`, (done) => {
         effect.subscribe(() => done());
         closeDbTimer$.next(0);
+    });
+
+    it(`and calls dbConnectionSource$.next with createClosingDbConnectionInfo if there is a db connection`, async () => {
+        const dbConnection: DbConnectionInfo = {
+            dbConnection: {}
+        };
+        dbConnectionSource$.next(dbConnection);
+        closeDbTimer$.next(0);
+        spyOn(dbConnectionSource$, "next").and.callThrough();
+        await effect.pipe(first()).toPromise();
+        expect(dbConnectionSource$.next).toHaveBeenCalledWith(createClosingDbConnectionInfo(dbConnection));
+
     });
 
 
