048307b85ac99850eb0270671fdcf8016ee43dfb
dgp-web <dgp-web@outlook.de>
2021-11-02 17:21:26 +0100

    Add TODOs to run-migrations$.function

--- src/functions/core/run-migrations$.function.ts
+++ src/functions/core/run-migrations$.function.ts
@@ -3,6 +3,7 @@ import * as _ from "lodash";
 import { AddEntityActionParamsMap } from "entity-store/src/models/composite-entity-action-payload.model";
 import { migrationConfig } from "../../constants";
 
+// TODO: Extract this default payload that receives an entity-db and a migrations object wherever it occurs
 export interface RunMigrations$Payload<TEntities extends MigrationEntities> {
     readonly db: EntityDb<TEntities>;
     readonly migrations: ReadonlyArray<Migration<any, any>>;
@@ -23,13 +24,17 @@ export async function runMigrations$<TEntities extends MigrationEntities>(
 
     let migrationInfos = Object.values(result.migrationInfo);
     migrationInfos = _.sortBy(migrationInfos, x => x.position);
+    // TODO: Adjust so we get the target position
+    // TODO: Extract getMaxPosition as default if no targetMigrationId is passed
+    // TODO: Extract getTargetMigrationPosition
+    // TODO: Add and test error handling as specified
     const maxPosition = migrationInfos.length > 0 ? _.max(migrationInfos.map(x => x.position)) : 0;
 
     const newMigrations = migrations.filter(migration => migration.position > maxPosition);
 
     for (const migration of newMigrations) {
         await migration.execute$({from: db, to: db});
-        await db.dispatch$({
+        await db.dispatch$({ // TODO: Extract
             add: {
                 migrationInfo: {
                     [migration.migrationId]: {
