2599edd8aa0f91909c51acd0b09d63f33fc592af
dgp-web <dgp-web@outlook.de>
2021-11-02 17:00:26 +0100

    Add targetMigrationId and disableAutoMigrations to EntityPouchDbPayload

--- src/models/entity-pouch-db-payload.model.ts
+++ src/models/entity-pouch-db-payload.model.ts
@@ -1,10 +1,13 @@
-import { MigrationEntities } from "./migration-entities.model";
-import { Many } from "data-modeling";
-import { Migration } from "./migration.model";
-import { PouchDbRef } from "./pouch-db-ref.model";
+import {MigrationEntities} from "./migration-entities.model";
+import {Many} from "data-modeling";
+import {Migration} from "./migration.model";
+import {PouchDbRef} from "./pouch-db-ref.model";
+import {MigrationId} from "./migration-id.model";
 
 export interface EntityPouchDbPayload<TEntityTypeMap extends MigrationEntities> {
-    readonly entityTypes: Many<keyof TEntityTypeMap>,
-    readonly dbRef: PouchDbRef,
-    readonly migrations?: ReadonlyArray<Migration<any, any>>
+    readonly entityTypes: Many<keyof TEntityTypeMap>;
+    readonly dbRef: PouchDbRef;
+    readonly migrations?: ReadonlyArray<Migration<any, any>>;
+    readonly targetMigrationId?: MigrationId;
+    readonly disableAutoMigrations?: boolean;
 }
