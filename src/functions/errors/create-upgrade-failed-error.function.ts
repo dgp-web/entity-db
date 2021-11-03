f01bed624d1e19f84404ecb46a1152279998ac0f
dgp-web <dgp-web@outlook.de>
2021-11-03 08:29:29 +0100

    Add createUpgradeFailedError

---
+++ src/functions/errors/create-upgrade-failed-error.function.ts
@@ -0,0 +1,15 @@
+export function createUpgradeFailedError(payload: {
+    readonly currentVersion: number;
+    readonly targetVersion: number;
+}): Error {
+
+    const currentVersion = payload.currentVersion;
+    const targetVersion = payload.targetVersion;
+
+    const message = `An upgrade is required because the target database version ${targetVersion} is below the current version ${currentVersion}.
+However, the needed migrations are not available. 
+Please ensure a) that the correct target version has been selected and b) that migrations for all versions have been provided.`
+
+    return Error(message);
+
+}
