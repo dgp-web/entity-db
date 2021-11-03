963beeddee3fc161d1244a1e6b012da18d1ab1f5
dgp-web <dgp-web@outlook.de>
2021-11-03 09:50:25 +0100

    Test getRequiredBackwardMigrationPositions

---
+++ src/functions/migrations/backward/__tests__/get-required-backward-migration-positions.function.spec.ts
@@ -0,0 +1,16 @@
+import {getRequiredBackwardMigrationPositions} from "../get-required-backward-migration-positions.function";
+
+describe("getRequiredBackwardMigrationPositions", () => {
+
+    it("should get all numbers between targetPosition and currentPosition (including the former)", () => {
+
+        const result = getRequiredBackwardMigrationPositions({
+            currentPosition: 3,
+            targetPosition: 1
+        });
+
+        expect(result).toEqual([2, 1]);
+
+    });
+
+});
