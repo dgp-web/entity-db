609dd6dd994ccb1eed98f237eebc01cbe07d6b88
dgp-web <dgp-web@outlook.de>
2022-03-03 16:10:56 +0100

    Fix error with backward migration

--- src/functions/migrations/backward/__tests__/get-backward-migrations.function.spec.ts
+++ src/functions/migrations/backward/__tests__/get-backward-migrations.function.spec.ts
@@ -1,7 +1,3 @@
-import {getCurrentMaxMigrationPosition} from "../../get-current-max-migration-position.function";
-import {testMigrationInfo} from "../../../../__tests__/constants/test-migration-info.constant";
-import {testMigrationInfo02} from "../../../../__tests__/constants/test-migration-info-02.constant";
-import {getForwardMigrations} from "../../forward/get-forward-migrations.function";
 import {testMigration} from "../../../../__tests__/constants/test-migration.constant";
 import {testMigration02} from "../../../../__tests__/constants/test-migration-02.constant";
 import {getBackwardMigrations} from "../get-backward-migrations.function";
@@ -14,7 +10,7 @@ describe("getBackwardMigrations", () => {
             targetPosition: 1,
             currentPosition: 2
         });
-        const expectedResult = [testMigration];
+        const expectedResult = [testMigration02];
         expect(result).toEqual(expectedResult);
     });
 
